28f3d5efb0a7bc271d1038e65e9a3a38059cbb9c
Praveen Wadhwani <PraveenW@citiustech.com>
2020-01-15 19:09:05 +0530

    Committed basic html file!

---
+++ app.py
@@ -0,0 +1,13 @@
+#import pandas as pd
+from flask import Flask, request, render_template, Response
+#import pickle
+
+# app
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
